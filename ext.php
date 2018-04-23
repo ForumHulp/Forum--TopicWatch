651d7ead1d109d7f5beb264d754899c37e801ac5
Forumhulp.com <info@forumhulp.com>
2018-04-23 09:24:07 +0200

    Opening brace

--- ext.php
+++ ext.php
@@ -35,26 +35,26 @@ class ext extends \phpbb\extension\base
 		switch ($old_state)
 		{
 			case '': // Empty means nothing has run yet
-				if (empty($old_state))
-				{
-					$this->container->get('user')->add_lang_ext('forumhulp/forumtopicwatch', 'info_acp_forumtopicwatch');
-					$this->container->get('template')->assign_var('L_EXTENSION_ENABLE_SUCCESS', $this->container->get('user')->lang['EXTENSION_ENABLE_SUCCESS'] .
-						(isset($this->container->get('user')->lang['FTWATCH_NOTICE']) ?
-							sprintf($this->container->get('user')->lang['FTWATCH_NOTICE'],
-									$this->container->get('user')->lang['ACP_CAT_MAINTENANCE'],
-									$this->container->get('user')->lang['ACP_FORUM_LOGS'],
-									$this->container->get('user')->lang['ACP_FORUM_TOPICWATCH']) : ''));
-				}
+			if (empty($old_state))
+			{
+				$this->container->get('user')->add_lang_ext('forumhulp/forumtopicwatch', 'info_acp_forumtopicwatch');
+				$this->container->get('template')->assign_var('L_EXTENSION_ENABLE_SUCCESS', $this->container->get('user')->lang['EXTENSION_ENABLE_SUCCESS'] .
+					(isset($this->container->get('user')->lang['FTWATCH_NOTICE']) ?
+						sprintf($this->container->get('user')->lang['FTWATCH_NOTICE'],
+								$this->container->get('user')->lang['ACP_CAT_MAINTENANCE'],
+								$this->container->get('user')->lang['ACP_FORUM_LOGS'],
+								$this->container->get('user')->lang['ACP_FORUM_TOPICWATCH']) : ''));
+			}
 
-				// Run parent enable step method
-				return parent::enable_step($old_state);
+			// Run parent enable step method
+			return parent::enable_step($old_state);
 
 			break;
 
 			default:
 
-				// Run parent enable step method
-				return parent::enable_step($old_state);
+			// Run parent enable step method
+			return parent::enable_step($old_state);
 
 			break;
 		}
